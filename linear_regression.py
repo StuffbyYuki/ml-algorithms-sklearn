88c09231050aa0b7d30947c673f8cb9c247ce4c0
stuffbyyuki <kake.19940627@gmail.com>
2020-12-22 17:22:10 -0700

    linear_regressoin.py added

--- linear_regression.py
+++ linear_regression.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import numpy as np
-from utilities import sklearn_data_to_df
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
+from utilities import sklearn_data_to_df, train_test_split_from_df
+from sklearn.datasets import load_boston, load_iris
 from sklearn.linear_model import LinearRegression
 
 # Get data
-iris = load_iris()
-df = sklearn_data_to_df(iris)
+data = load_boston()
+df = sklearn_data_to_df(data)
 
 # Split data
+X_train, X_test, y_train, y_test = train_test_split_from_df(df, 'target', test_size=0.2, random_state=0)
+print(len(X_train), len(X_test), len(y_train), len(y_test))
 
 # Build a prediction model
+linear_model = LinearRegression().fit(X_train, y_train)
 
+# Get results
+print(linear_model.get_params())
+print(linear_model.coef_)
+print(linear_model.intercept_)
+print(linear_model.predict(X_test))
+print(linear_model.score(X_test, y_test))
--- utilities.py
+++ utilities.py
@@ -1,9 +1,31 @@
 # Module that contains functions that can be used across all files
+# df represents pandas DataFrame
+
 import pandas as pd
 import numpy as np
-
 def sklearn_data_to_df(data):
-    '''translate sklearn df to a pandas dataframe'''
+    '''
+        translate sklearn df to a pandas dataframe
+        dependencies: numpy, pandas
+    '''
     pandas_df = pd.DataFrame(data=np.c_[data['data'], data['target']],
-                        columns=data['feature_names'] + ['target'])
+                        columns=np.append(data['feature_names'], ['target']))
+    print(pandas_df.head())
     return pandas_df
+
+from sklearn.model_selection import train_test_split
+def train_test_split_from_df(df, target_col, test_size=None, train_size=None, random_state=None, shuffle=True, stratify=None):
+    '''
+        split df into train and test
+        utilizing sklearn "train_test_split"
+        returns a tuple
+    '''
+    X = df.drop(target_col, axis=1)
+    y = df[target_col]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                        test_size=test_size, 
+                                                        train_size=train_size,
+                                                        random_state=random_state,
+                                                        shuffle=shuffle,
+                                                        stratify=stratify)
+    return X_train, X_test, y_train, y_test
